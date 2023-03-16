3256888db269da5d18a0810eccc72a4157de2fb9
Rafael Moreira <rafael16gsm@gmail.com>
2023-03-16 20:10:02 -0300

    Reestruturação do código

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const botaoFormulario = document.querySelector('#botao-form')
 const emailFormulario = document.querySelector('#email')
+const elementWarningText = document.querySelector('.warning-text')
 
 botaoFormulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
 
     if (emailFormulario.value == "") {
-        const elementWarningText = document.querySelector('.warning-text')
         elementWarningText.innerText = "Preencha este campo com um e-mail válido."
 
     } else {
         alert(`E-mail '${emailFormulario.value}' cadastrado com sucesso!`)
-
-        const elementWarningText = document.querySelector('.warning-text')
         elementWarningText.innerText = ""
     }
 
